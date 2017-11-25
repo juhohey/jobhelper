530141db95791ee6c854b0b2a7dfc643338065cf
Adam West <jsarry@gmail.com>
2017-11-25 13:34:24 +0200

    add missing imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ProfileHome from '@/components/pages/ProfileHome'
+import ProfileSubject from '@/components/pages/ProfileSubject'
+import ProfileHobbies from '@/components/pages/ProfileHobbies'
+
 
 Vue.use(Router)
 
@@ -27,4 +31,4 @@ export default new Router({
             component: ProfileHobbies
         }
     ]
-}) 
+})
