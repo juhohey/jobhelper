3dd85b19d19c4f930ee59605971a305d8ca3c05e
nickyfoster <nik9516@gmail.com>
2017-11-25 21:08:14 +0200

    API

--- src/data_api.js
+++ src/data_api.js
@@ -1,3 +1,8 @@
+// API's
+
+// ESCO's API
+// API - get 7 essential skills for occupation (this ex. - 'Industrial Designer')
+
 fetch('https://ec.europa.eu/esco/api/resource/occupation?uri=http://data.europa.eu/esco/occupation/ab7bccb2-6f81-4a3d-a0c0-fca5d47d2775', {
 	method: 'get'
 }).then(function(response) {
@@ -8,4 +13,16 @@ fetch('https://ec.europa.eu/esco/api/resource/occupation?uri=http://data.europa.
 	console.error(err)
 });
 
-//console.log(response.jobCategories.map((title)=>title.name))
+// Duunitori's API
+// API - get total number of jobs in the city
+var city = 'Lappeenranta'
+
+fetch(`https://duunitori.fi/api/v1/5d3fc27dec93f5e5105e3443edfc421bb57c3603/jobentries?apply=&applyclicks_sum__gte=&applyclicks_sum__lte=&applyclicks_target_met=&area=${city}&company=&company_code=&date_created_after=&date_created_before=&days_left__gte=&days_left__lte=&format=json&pageviews_sum__gte=&pageviews_sum__lte=&pageviews_target_met=&publish_level=&search=&search_also_descr=&source=&tag=&targets_met=`, {
+	method: 'get'
+}).then(function(response) {
+	return response.json()
+}).then(function(response) {
+	console.log(response)
+}).catch(function(err) {
+	console.error(err)
+});
