b912e663ce442df6a404e2945ee90f6bedd8ab3e
Adam West <jsarry@gmail.com>
2017-11-25 12:42:54 +0200

    add profile routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,26 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/profile',
+            name: 'ProfileHome',
+            component: ProfileHome
+        },
+        {
+            path: 'profile/subjects',
+            name: 'ProfileSubject',
+            component: ProfileSubject
+        },
+        {
+            path: 'profile/hobbies',
+            name: 'ProfileHobbies',
+            component: ProfileHobbies
+        }
+    ]
 })
